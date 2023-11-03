268a36cae5894755f8d32e037e65998b5aa3415b
Jongmin kim <47784464+miniwa00@users.noreply.github.com>
2023-11-04 00:37:48 +0900

    Refactor preprocessing code to remove punctuation and clean text

--- mbti_preprocessing.py
+++ mbti_preprocessing.py
@@ -1,91 +1,69 @@
 import pandas as pd
 import re
 from datetime import datetime
+from glob import glob
 
-# 함수 정의
+
+def remove_punctuation(input_string):
+    # 문자열에서 기호를 제거하는 정규 표현식 패턴
+    pattern = r"[^\uAC00-\uD7A30-9a-zA-Z\s]"
+    # 정규 표현식 패턴을 사용하여 기호를 공백으로 대체
+    try:
+        result = re.sub(pattern, "", input_string)
+    except:
+        result = input_string
+    return result
+
+
 def clean_and_save_data(input_file_path):
-    # 파일 불러오기
     comment = pd.read_csv(input_file_path)
-    mbti = comment['mbti']
-    comments = comment['comments']
+    mbti = comment["mbti"]
+    comments = comment["comments"]
     new_text = []
-
-    # 기호 제거
-    def remove_punctuation(input_string):
-        # 문자열에서 기호를 제거하는 정규 표현식 패턴
-        pattern = r'[^\uAC00-\uD7A30-9a-zA-Z\s]'
-        # 정규 표현식 패턴을 사용하여 기호를 공백으로 대체
-        try:
-            result = re.sub(pattern, '', input_string)
-        except:
-            result = input_string
-        return result
-
-    # 변환
     for i in comments:
         cleaned_string = remove_punctuation(i)
         new_text.append(cleaned_string)
-
     # 줄바꿈 제거
-    result = pd.DataFrame({'mbti': mbti, 'comments': new_text})
-    result['comments'] = result['comments'].str.replace('\s+', ' ', regex=True)
-
+    result = pd.DataFrame({"mbti": mbti, "comments": new_text})
+    result["comments"] = result["comments"].str.replace("\s+", " ", regex=True)
     return result
 
-# 함수 정의
+
 def clean_and_save_data2(input_file_path):
-
-    # 파일 불러오기
     post = pd.read_csv(input_file_path)
-    mbti = post['mbti']
-    text = post['text']
+    mbti = post["mbti"]
+    text = post["text"]
     new_text = []
-
-    # 기호 제거
-    def remove_punctuation(input_string):
-        # 문자열에서 기호를 제거하는 정규 표현식 패턴
-        pattern = r'[^\uAC00-\uD7A30-9a-zA-Z\s]'
-        # 정규 표현식 패턴을 사용하여 기호를 공백으로 대체
-        try:
-            result = re.sub(pattern, '', input_string)
-        except:
-            result = input_string
-        return result
-
-    # 변환
     for i in text:
         cleaned_string = remove_punctuation(i)
         new_text.append(cleaned_string)
-
-    # 줄바꿈 제거
-    result = pd.DataFrame({'mbti': mbti, 'text': new_text})
-    result['text'] = result['text'].str.replace('\s+', ' ', regex=True)
-
+    result = pd.DataFrame({"mbti": mbti, "text": new_text})
+    result["text"] = result["text"].str.replace("\s+", " ", regex=True)
     return result
 
-# 처리할 파일 목록
-input_files_comment = ['./data/labeled_comment/labeled_comment_2023-11-02_18-52-33.csv']
-input_files_post = ['./data/labeled_post/labeled_post_2023-11-02_18-52-33.csv']
 
-# 결과를 저장할 빈 데이터프레임 생성
-final_result_comment = pd.DataFrame(columns=['mbti', 'comments'])
-final_result_post = pd.DataFrame(columns=['mbti','text'])
+if __name__ == "__main__":
+    input_files_comment = glob("./data/raw_comment/*.csv")
+    input_files_post = glob("./data/raw_post/*.csv")
 
-# 각 파일을 정제하고 결과를 빈 데이터프레임에 추가
-for input_file in input_files_comment:
-    cleaned_data_comment = clean_and_save_data(input_file)
-    final_result_comment = pd.concat([final_result_comment, cleaned_data_comment])
+    # 결과를 저장할 빈 데이터프레임 생성
+    final_result_comment = pd.DataFrame(columns=["mbti", "comments"])
+    final_result_post = pd.DataFrame(columns=["mbti", "text"])
 
-for input_file in input_files_post:
-    cleaned_data_post = clean_and_save_data2(input_file)
-    final_result_post = pd.concat([final_result_post, cleaned_data_post])
+    # 각 파일을 정제하고 결과를 빈 데이터프레임에 추가
+    for input_file in input_files_comment:
+        cleaned_data_comment = clean_and_save_data(input_file)
+        final_result_comment = pd.concat([final_result_comment, cleaned_data_comment])
 
-# 모든 결과를 하나의 CSV 파일로 저장
+    for input_file in input_files_post:
+        cleaned_data_post = clean_and_save_data2(input_file)
+        final_result_post = pd.concat([final_result_post, cleaned_data_post])
 
-time = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    # 모든 결과를 하나의 CSV 파일로 저장
+    time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
-comments_name = f'./data/pre_comment/pre_comment_{time}.csv'
-post_name = f'./data/pre_post/pre_post_{time}.csv'
+    comments_name = f"./data/pre_comment/pre_comment_{time}.csv"
+    post_name = f"./data/pre_post/pre_post_{time}.csv"
 
-final_result_comment.to_csv(comments_name, index=False)
-final_result_post.to_csv(post_name, index=False)
+    final_result_comment.to_csv(comments_name, index=False)
+    final_result_post.to_csv(post_name, index=False)
