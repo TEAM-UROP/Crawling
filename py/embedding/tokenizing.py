9d92a72cf2e718d2c13fd09b7e1928239a237fa8
Jongmin kim <47784464+miniwa00@users.noreply.github.com>
2023-11-28 21:58:51 +0900

    Refactor tokenizing logic in Tokenizer class

--- py/embedding/tokenizing.py
+++ py/embedding/tokenizing.py
@@ -1,16 +1,19 @@
 import pandas as pd
 from konlpy.tag import Kkma, Okt, Hannanum
+from embedding import Embedding
 
 
-class Preprocessor:
-    def __init__(self, data):
-        self.data = pd.read_csv(data, encoding="utf-8")
+class Tokenizer:
+    def __init__(self, args):
+        self.args = args
+        self.data = pd.read_csv(self.args.data, encoding="utf-8")
         self.okt = Okt()
         self.kkma = Kkma()
         self.hannanum = Hannanum()
 
     def tokenizing(self):
         self.data["comments"] = self.data["comments"].astype(str)
+
         tok_okt_morphs = self.data["comments"].apply(
             lambda row: " ".join(self.okt.morphs(row))
         )
@@ -21,13 +24,13 @@ class Preprocessor:
         #     )
         # )
 
-        # tok_okt_nouns = self.data["comments"].apply(
-        #     lambda row: " ".join(self.okt.nouns(row))
-        # )
+        tok_okt_nouns = self.data["comments"].apply(
+            lambda row: " ".join(self.okt.nouns(row))
+        )
 
-        # tok_kkma_morphs = self.data["comments"].apply(
-        #     lambda row: " ".join(self.kkma.morphs(row))
-        # )
+        tok_kkma_morphs = self.data["comments"].apply(
+            lambda row: " ".join(self.kkma.morphs(row))
+        )
 
         # tok_kkma_pos = self.data["comments"].apply(
         #     lambda row: " ".join(
@@ -35,13 +38,13 @@ class Preprocessor:
         #     )
         # )
 
-        # tok_kkma_nouns = self.data["comments"].apply(
-        #     lambda row: " ".join(self.kkma.nouns(row))
-        # )
+        tok_kkma_nouns = self.data["comments"].apply(
+            lambda row: " ".join(self.kkma.nouns(row))
+        )
 
-        # tok_hannaum_morphs = self.data["comments"].apply(
-        #     lambda row: " ".join(self.hannanum.morphs(row))
-        # )
+        tok_hannaum_morphs = self.data["comments"].apply(
+            lambda row: " ".join(self.hannanum.morphs(row))
+        )
 
         # tok_hannaum_pos = self.data["comments"].apply(
         #     lambda row: " ".join(
@@ -49,28 +52,29 @@ class Preprocessor:
         #     )
         # )
 
-        # tok_hannaum_nouns = self.data["comments"].apply(
-        #     lambda row: " ".join(self.hannanum.morphs(row))
-        # )
+        tok_hannaum_nouns = self.data["comments"].apply(
+            lambda row: " ".join(self.hannanum.morphs(row))
+        )
 
         res = (
             tok_okt_morphs,
             # tok_okt_pos,
-            # tok_okt_nouns,
-            # tok_kkma_morphs,
+            tok_okt_nouns,
+            tok_kkma_morphs,
             # tok_kkma_pos,
-            # tok_kkma_nouns,
-            # tok_hannaum_morphs,
+            tok_kkma_nouns,
+            tok_hannaum_morphs,
             # tok_hannaum_pos,
-            # tok_hannaum_nouns,
+            tok_hannaum_nouns,
         )
 
         return res
 
 
 if __name__ == "__main__":
-    pre = Preprocessor("./data/sample_comment.csv")
-    res = pre.tokenizing()
-    for idx, i in enumerate(res):
-        i.to_csv(f"tokenized_{idx}.csv", index=False, encoding="utf-8-sig")
-        break
+    # pre = Tokenizer()
+    # res = pre.tokenizing()
+    # for idx, i in enumerate(res):
+    #     i.to_csv(f"tokenized_{idx}.csv", index=False, encoding="utf-8-sig")
+    #     break
+    embedding = Embedding("./tokenized_0.csv")
