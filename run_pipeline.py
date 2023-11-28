33c2af17caa224b728d1ec837c465224a60c474a
Jongmin kim <47784464+miniwa00@users.noreply.github.com>
2023-11-28 21:58:26 +0900

    Refactor run_pipeline.py arguments

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,24 +1,19 @@
 import argparse
 from tokenizing import Tokenizer
 from embedding import Embedding
-from base_modeling import TextModel
+
+# from base_modeling import TextModel
 
 
 def get_args_parser():
     parser = argparse.ArgumentParser(description="Run the pipeline")
-    parser.add_argument(
-        "--data",
-        type=str,
-        default="data/sample_comment.csv",
-        help="Path to the data folder",
-    )
-    parser.add_argument("--seed", type=int, default=42, help="Random seed")
-    parser.add_argument(
-        "--device", type=str, default="cuda", help="Device to run the code"
-    )
-    parser.add_argument("--batch_size", type=int, default=32, help="Batch size")
-    parser.add_argument("--num_epochs", type=int, default=10, help="Number of epochs")
-    parser.add_argument("--lr", type=float, default=1e-3, help="Learning rate")
+    parser.add_argument("--data", type=str, default="data/sample_comment.csv")
+    parser.add_argument("--seed", type=int, default=42)
+    parser.add_argument("--device", type=str, default="cuda")
+    parser.add_argument("--batch_size", type=int, default=32)
+    parser.add_argument("--num_epochs", type=int, default=10)
+    parser.add_argument("--lr", type=float, default=1e-3)
+    parser.add_argument("--sp_option", type=str, default=True)
     return parser
 
 
