6a41629bfddacb4bca54a988a8308904ee407d33
miniwa00 <miniwa00@gmail.com>
2023-12-03 21:48:33 +0900

    Refactor pipeline script and update dependencies

--- py/embedding/run_pipeline.py
+++ py/embedding/run_pipeline.py
@@ -1,14 +1,14 @@
 import argparse
-
-# sys.path.append("./py/embedding ")
 from tokenizing import Tokenizer
 from embedding import Embedding
 from base_modeling import TextModel
+import pandas as pd
 
 
 def get_args_parser():
     parser = argparse.ArgumentParser(description="Run the pipeline")
     parser.add_argument("--data", type=str, default="data/sample_comment.csv")
+    parser.add_argument("--label", type=str)
     parser.add_argument("--seed", type=int, default=42)
     parser.add_argument("--device", type=str, default="cuda")
     parser.add_argument("--batch_size", type=int, default=32)
@@ -21,41 +21,41 @@ def get_args_parser():
 if __name__ == "__main__":
     parser = get_args_parser()
     args = parser.parse_args()
-
     preprocessor = Tokenizer(args)
     res = preprocessor.tokenizing()
 
     for tokenized_sentences in res:
         embedding = Embedding(args, tokenized_sentences)
-        vectors = embedding.get_embedding_vector()
-        textmodel = TextModel(args, vectors)
+        w2v_model = embedding.get_embedding_model()
+        print(w2v_model.wv)
+        textmodel = TextModel(args, w2v_model)
         textmodel.train_model()
-import argparse
-from tokenizing import Tokenizer
-from embedding import Embedding
 
-# from base_modeling import TextModel
-
-
-def get_args_parser():
-    parser = argparse.ArgumentParser(description="Run the pipeline")
-    parser.add_argument("--data", type=str, default="data/sample_comment.csv")
-    parser.add_argument("--seed", type=int, default=42)
-    parser.add_argument("--device", type=str, default="cuda")
-    parser.add_argument("--batch_size", type=int, default=32)
-    parser.add_argument("--num_epochs", type=int, default=10)
-    parser.add_argument("--lr", type=float, default=1e-3)
-    parser.add_argument("--sp_option", type=str, default=False)
-    return parser
+# import argparse
+# from tokenizing import Tokenizer
+# from embedding import Embedding
+# # from base_modeling import TextModel
 
 
-if __name__ == "__main__":
-    parser = get_args_parser()
-    args = parser.parse_args()
+# def get_args_parser():
+#     parser = argparse.ArgumentParser(description="Run the pipeline")
+#     parser.add_argument("--data", type=str, default="data/sample_comment.csv")
+#     parser.add_argument("--seed", type=int, default=42)
+#     parser.add_argument("--device", type=str, default="cuda")
+#     parser.add_argument("--batch_size", type=int, default=32)
+#     parser.add_argument("--num_epochs", type=int, default=10)
+#     parser.add_argument("--lr", type=float, default=1e-3)
+#     parser.add_argument("--sp_option", type=str, default=False)
+#     return parser
 
-    preprocessor = Tokenizer(args)
-    res = preprocessor.tokenizing()
 
-    for tokenized_sentences in res:
-        embedding = Embedding(args, tokenized_sentences)
-        word_vectors = embedding.get_embedding_vector()
+# if __name__ == "__main__":
+#     parser = get_args_parser()
+#     args = parser.parse_args()
+
+#     preprocessor = Tokenizer(args)
+#     res = preprocessor.tokenizing()
+
+#     for tokenized_sentences in res:
+#         embedding = Embedding(args, tokenized_sentences)
+#         word_vectors = embedding.get_embedding_vector()
